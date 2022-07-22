df4651e7f94de9b06f292fbd05af46c01a34dd2f
Farid Attamimi <tamimi2021farid@gmail.com>
2022-07-22 12:14:08 +0200

    Back-End

---
+++ main.py
@@ -0,0 +1,43 @@
+# Do not modify these lines
+__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
+__human_name__ = 'operators'
+
+# Add your code after this line
+'''spain_total_area = 505307
+switzerland_total_area = 41277
+print(spain_total_area > switzerland_total_area)
+'''
+# 1
+most_langguage_inSpain = "Castilian Spanish"
+most_langguage_in_Switzerland = "German"
+print(most_langguage_inSpain == most_langguage_in_Switzerland)
+
+# 2
+most_prevalentReligion_inSpain = "Roman Catholic"
+most_prevalentReligion_inSwitzerland = "Roman Catholic"
+print(most_prevalentReligion_inSpain == most_prevalentReligion_inSwitzerland)
+
+# 3
+name_length_spainsCapital = len("Madrid")
+name_length_switzerlandsCapital = len("Bern")
+print(name_length_spainsCapital != name_length_switzerlandsCapital)
+
+# 4
+switzerland_gdp = 731.502
+spain_gdp = 1.393
+print(switzerland_gdp < spain_gdp)
+
+# 5
+population_growth_Spain = 0.13
+population_growth_Switzerland = 0.65
+print(population_growth_Spain < 1 and population_growth_Switzerland < 1)
+
+# 6
+population_count_Spain = 47163418
+population_count_Switzerland = 8608698
+print(population_count_Spain > 10000000 or population_count_Switzerland > 10000000)
+
+# 7
+population_in_Spain = 4735
+population_in_Switzerland = 8637
+print(not(population_in_Spain > 10000000 and population_in_Switzerland > 10000000))
